f7c326b4650349021abf6e2071f1ebe4310bdac8
Renda02 <64105005+Renda02@users.noreply.github.com>
2020-06-09 03:13:09 +0200

    Added files

---
+++ src/app.js
@@ -0,0 +1,112 @@
+//Format date
+let dateElement = document.querySelector("#date");
+let now = new Date();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let date = now.getDate();
+let min = now.getMinutes();
+if (min < 10) {
+  min = `0${min}`;
+}
+let year = now.getFullYear();
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let monthly = months[now.getMonth()];
+let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+let days = weekDays[now.getDay()];
+dateElement.innerHTML = `${days},${monthly} ${date} ${year},${hours}:${min}`;
+
+//Location
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#tempToday").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#feels").innerHTML = response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed * 3.6
+  ); //converting m/s to km/h
+  document.querySelector("#feelsLike").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+}
+
+function searchCity(city) {
+  let apiKey = "a5dc471873d618b50635e979e6f6c8fc";
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#txt").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "a5dc471873d618b50635e979e6f6c8fc";
+  let long = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function findLocation() {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-box");
+searchForm.addEventListener("submit", handleSearch);
+
+let currentLocationButton = document.querySelector("#btn");
+currentLocationButton.addEventListener("click", findLocation);
+searchCity("Clarens");
+//Unit Conversion
+
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fTemp = (cTemp * 9) / 5 + 32;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let currentTemperature = document.querySelector("#tempToday");
+  currentTemperature.innerHTML = Math.round(fTemp) + "° F";
+}
+
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let currentTemperature = document.querySelector("#tempToday");
+  currentTemperature.innerHTML = Math.round(cTemp) + "° C";
+}
+
+let cTemp = null;
+
+let fahrenheitLink = document.querySelector("#fahrenht");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
